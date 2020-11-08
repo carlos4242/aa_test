688f06219118bcb88ad55d7c4531087d2cae8d31
Carl Peto <carl.peto@me.com>
2020-11-08 15:24:46 +0000

    Initial Commit

---
+++ Brumcast/BrumcastApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrumcastApp.swift
+//  Brumcast
+//
+//  Created by Carl Peto on 08/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct BrumcastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
